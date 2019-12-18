f3d498b37cf05bad93f680cab5bef4a9178df691
George Mann <george.m.mann@gmail.com>
2019-12-18 14:42:49 -0500

    .

---
+++ python-everything/money/m01_01_basic.py
@@ -0,0 +1,9 @@
+balance = 1000
+name = "George Mann"
+account_no = "01123581321"
+
+
+print("name:", name, "    account:", account_no, "    balance:", balance)
+print("name:", name, "    account:", account_no, "    balance:", "$" + str(balance))
+print("name:", name, "    account:", account_no, "    balance:", "$" + str(float(balance)))
+print("\n") 
